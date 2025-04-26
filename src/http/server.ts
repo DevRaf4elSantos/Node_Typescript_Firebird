75f6f4de566ebdf0830b3c2b588b8691a0d9e9a7
DevRaf4elSantos <contatodevrafaelsantos@gmail.com>
2025-04-26 18:14:54 -0300

    criando servidor com express

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,1 +1,19 @@
- 
+import express from "express";
+import cors  from "cors";
+
+const app = express();
+
+// Middleware - Permite o uso do json
+app.use(express.json());
+
+// Middleware - Cors
+app.use(cors())
+
+app.get('/', (req, res) => {
+res.status(200).send("Retornar lista de produtos, teste")
+    
+});
+
+app.listen(3000, () => {
+    console.log("Servidor online")
+})
